c9bb72dcd427ae8b85461d770c5f997d00b4fec5
Louie Soulen Kurenai <168345503+LouieNotHere@users.noreply.github.com>
2024-05-17 15:19:32 +0800

    Update PerformanceCalculator.java

--- src/com/rian/difficultycalculator/calculator/PerformanceCalculator.java
+++ src/com/rian/difficultycalculator/calculator/PerformanceCalculator.java
@@ -164,7 +164,7 @@ public class PerformanceCalculator {
         
         // no more comments, pure stress to peeps
         if (difficultyAttributes.mods.contains(GameMod.MOD_RELAX)) {
-            aimValue *= 1.325 + (difficultyAttributes.approachRate * 0.004) + (lengthBonus / 31415.926535);
+            aimValue *= 1.325 + (difficultyAttributes.approachRate * 0.004);
             if (difficultyAttributes.mods.contains(GameMod.MOD_HARDROCK)) {
                 aimValue *= 1.05 * (difficultyAttributes.overallDifficulty / 105);
             }
